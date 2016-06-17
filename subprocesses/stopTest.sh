5b2825b925a8367dc9acc64ae98043c9cbcbe6fa
riverbedintern4lyfe <bullsfan12345678@yahoo.com>
2016-06-17 13:09:38 -0700

    Delete stopTest.sh

--- subprocesses/stopTest.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-
-controllers=$(fuel node | grep ' controller' | grep '10.20.0.[0-9]*' -o)
-	for machine in ${controllers}; do
-		ssh root@$machine 'bash -s' < /etc/A-B-Testing/subprocesses/stopTest.sh 2>/dev/null
-	done 
